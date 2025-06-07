767c2db0b526c4dfcfff25421127dce8d910e571
ErjieIsHeree <erjieisheree@gmail.com>
2025-06-07 18:32:38 +0200

    More cleaning

--- YonuncaCONIA_Deprecated/app/src/main/java/com/erjieisheree/yonuncaconia/ui/navigation/AppScreens.kt
+++
@@ -1,8 +0,0 @@
-package com.erjieisheree.yonuncaconia.ui.navigation
-
-sealed class AppScreens(val route: String) {
-    object FirstScreen: AppScreens("init")
-    object SecondScreen: AppScreens("home")
-    object ThirdScreen: AppScreens("pregame")
-    object FourthScreen: AppScreens("game")
-}
